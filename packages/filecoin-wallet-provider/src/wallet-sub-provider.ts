05c2481aab5c3d3c67ec6de5bec5cbff55cb631f
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-10-28 14:44:44 -0400

    Cleanup

--- packages/filecoin-wallet-provider/src/wallet-sub-provider.ts
+++ packages/filecoin-wallet-provider/src/wallet-sub-provider.ts
@@ -1,7 +1,8 @@
 import { LotusMessage, SignedLotusMessage } from '@glif/filecoin-message'
+import { Network } from './utils/network'
 
 export interface WalletSubProvider {
   newAccount?(): Promise<string>
-  getAccounts(): Promise<string[]>
+  getAccounts(nStart: number, nEnd: number, network: Network): Promise<string[]>
   sign(from: string, message: LotusMessage): Promise<SignedLotusMessage>
 }
