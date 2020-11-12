d05f60edaeed82d8b3904afc6c27c3e2d031430d
Sergey Ukustov <sergey@ukstv.me>
2020-11-12 15:38:32 +0400

    feat: use closure to store private key
    Eliminates possibility of the private key leaking.

--- packages/local-managed-provider/src/local-managed-provider.ts
+++ packages/local-managed-provider/src/local-managed-provider.ts
@@ -1,29 +1,16 @@
 import { Network, WalletSubProvider } from '@glif/filecoin-wallet-provider'
 import { SignedLotusMessage } from '@glif/filecoin-message'
-import { ExtendedKey, MessageParams } from '@zondax/filecoin-signing-tools'
-import uint8arrays from 'uint8arrays'
-
-const moduleToImport = process.env.JEST_WORKER_ID
-  ? "@zondax/filecoin-signing-tools/nodejs"
-  : "@zondax/filecoin-signing-tools";
-// tslint:disable-next-line:no-var-requires
-const signingTools = require(moduleToImport);
+import { MessageParams } from '@zondax/filecoin-signing-tools'
+import {
+  privateKeyContainer,
+  PrivateKeyContainer,
+} from './private-key-container'
 
 export class LocalManagedProvider implements WalletSubProvider {
-  private readonly privateKey: ExtendedKey
+  private readonly privateKey: PrivateKeyContainer
 
   constructor(privateKey: string, network: Network) {
-    const bytes = uint8arrays.fromString(privateKey, 'base16')
-    const json = JSON.parse(uint8arrays.toString(bytes))
-    const keyType = json.Type
-    const testnet = network === Network.TEST
-    if (keyType === 'bls') {
-      this.privateKey = signingTools.keyRecoverBLS(json.PrivateKey, testnet)
-    } else if (keyType === 'secp256k1') {
-      this.privateKey = signingTools.keyRecover(json.PrivateKey, testnet)
-    } else {
-      throw new Error(`Unknown key type: ${keyType}`)
-    }
+    this.privateKey = privateKeyContainer(privateKey, network)
   }
 
   async getAccounts(): Promise<string[]> {
@@ -35,11 +22,7 @@ export class LocalManagedProvider implements WalletSubProvider {
     message: MessageParams,
   ): Promise<SignedLotusMessage> {
     if (from === this.privateKey.address) {
-      const asString = await signingTools.transactionSignLotus(
-        message,
-        this.privateKey.private_base64,
-      )
-      return JSON.parse((asString as unknown) as string)
+      return this.privateKey.sign(message)
     } else {
       throw new Error(`Can only sign with address ${this.privateKey.address}`)
     }
---
+++ packages/local-managed-provider/src/private-key-container.ts
@@ -0,0 +1,44 @@
+import { Network } from '@glif/filecoin-wallet-provider'
+import uint8arrays from 'uint8arrays'
+import { ExtendedKey, MessageParams } from '@zondax/filecoin-signing-tools'
+import { SignedLotusMessage } from '@glif/filecoin-message'
+
+const moduleToImport = process.env.JEST_WORKER_ID
+  ? '@zondax/filecoin-signing-tools/nodejs'
+  : '@zondax/filecoin-signing-tools'
+// tslint:disable-next-line:no-var-requires
+const signingTools = require(moduleToImport)
+
+export interface PrivateKeyContainer {
+  address: string
+  sign(message: MessageParams): Promise<SignedLotusMessage>
+}
+
+export function privateKeyContainer(
+  privateKey: string,
+  network: Network,
+): PrivateKeyContainer {
+  const bytes = uint8arrays.fromString(privateKey, 'base16')
+  const json = JSON.parse(uint8arrays.toString(bytes))
+  const keyType = json.Type
+  const testnet = network === Network.TEST
+  let extendedKey: ExtendedKey
+  if (keyType === 'bls') {
+    extendedKey = signingTools.keyRecoverBLS(json.PrivateKey, testnet)
+  } else if (keyType === 'secp256k1') {
+    extendedKey = signingTools.keyRecover(json.PrivateKey, testnet)
+  } else {
+    throw new Error(`Unknown key type: ${keyType}`)
+  }
+
+  return {
+    address: extendedKey.address,
+    sign: async (message: MessageParams) => {
+      const asString = await signingTools.transactionSignLotus(
+        message,
+        extendedKey.private_base64,
+      )
+      return JSON.parse((asString as unknown) as string)
+    },
+  }
+}
