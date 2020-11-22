90f77df5e96359ff384779eca6a536403f8f3b51
Bret Comnes <bcomnes@gmail.com>
2020-11-22 12:52:45 -0700

    Port NetworkSwitcher and add tests and story
    This one needed to be modified slightly.  Just move some of the handler functions to the consuming parent.

--- packages/react-components/src/constants.js
+++ packages/react-components/src/constants.js
@@ -5,6 +5,10 @@ export const IMPORT_MNEMONIC = 'IMPORT_MNEMONIC'
 export const CREATE_MNEMONIC = 'CREATE_MNEMONIC'
 export const IMPORT_SINGLE_KEY = 'IMPORT_SINGLE_KEY'
 
+/* NETWORK VARS */
+export const MAINNET = 'f'
+export const TESTNET = 't'
+
 /* TX METHOD TYPES */
 export const SEND = 'SEND'
 export const PROPOSE = 'PROPOSE'
