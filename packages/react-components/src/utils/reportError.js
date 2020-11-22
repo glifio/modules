5b4f1f1148fcb292d80d22aa32b37704f94817e6
Bret Comnes <bcomnes@gmail.com>
2020-11-22 12:32:58 -0700

    Port NodeConnected and tests, add Stories

---
+++ packages/react-components/src/utils/reportError.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import Router from 'next/router'
+
+// This simply formats error messages and sends them to our slack channel
+export default async (id, shouldSendToErrorPage, ...args) => {
+  /* eslint-disable-next-line no-console */
+  console.log('...args', ...args)
+  if (process.env.IS_PROD) {
+    await axios.post(
+      'https://errors.glif.io',
+      JSON.stringify({ text: `WALLET:${id}` })
+    )
+  } else {
+    /* eslint-disable-next-line no-console */
+    console.error(id, ...args)
+  }
+  if (shouldSendToErrorPage) Router.push('/error/wallet-down')
+}
