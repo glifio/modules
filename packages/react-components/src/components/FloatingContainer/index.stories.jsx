971e4bbcb798a8270b7edc3b04cea9ed5fd519a4
Bret Comnes <bcomnes@gmail.com>
2020-11-22 11:16:17 -0700

    Port FloatingContainer, add story and test

---
+++ packages/react-components/src/components/FloatingContainer/index.stories.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import theme from '../theme'
+import ThemeProvider from '../ThemeProvider'
+
+import FloatingContainer from './index'
+
+export default {
+  title: 'FloatingContainer/FloatingContainer',
+  component: FloatingContainer,
+  decorators: [
+    Story => (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <ThemeProvider theme={theme}>{Story()}</ThemeProvider>
+      </div>
+    )
+  ],
+  parameters: { actions: { argTypesRegex: '^on.*' } }
+}
+
+const Template = args => <FloatingContainer {...args} />
+
+export const Base = Template.bind({})
+Base.args = {
+  children: <div>Hello world</div>
+}
