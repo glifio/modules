0b2276e1b46c2e869602df727165b2f6f8a17a14
Eric Chaves <erchaves@gmail.com>
2021-09-08 14:47:20 -0400

    Fix style of buttons. Give them set heights.

--- packages/react-components/src/components/Link/ButtonLink.jsx
+++ packages/react-components/src/components/Link/ButtonLink.jsx
@@ -6,13 +6,16 @@ const StyledNavLink = styled(StyledATag).attrs(props => ({
   px: 3,
   py: 2,
   mx: 1,
+  height: 6,
   fontSize: 2,
   cursor: props.disabled ? 'not-allowed' : 'pointer',
   color: props.theme.colors.buttons.secondary.borderColor
 }))`
   /* We couldn't set the border above. So add it here instead. */
   border: 1px solid;
-  border-radius: 1em;
+  display: flex;
+  align-items: center;
+  border-radius: ${props => props.theme.sizes[6]}px;
   border-color: ${props => props.theme.colors.buttons.secondary.borderColor};
   color: ${props => props.theme.colors.buttons.secondary.borderColor};
   transition: 0.18s ease-in-out;
