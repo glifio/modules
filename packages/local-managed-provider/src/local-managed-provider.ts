790c92aff58774ca86c83bcff4b668fc96ba931d
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-11-23 14:32:02 -0500

    Upgrade single key provider due to upstream breaking changes

--- packages/local-managed-provider/src/local-managed-provider.ts
+++ packages/local-managed-provider/src/local-managed-provider.ts
@@ -1,27 +1,56 @@
-import { WalletSubProvider } from '@glif/filecoin-wallet-provider'
+import { WalletSubProvider, errors } from '@glif/filecoin-wallet-provider'
 import { LotusMessage, SignedLotusMessage } from '@glif/filecoin-message'
-import { Network } from '@glif/filecoin-address'
+import { CoinType } from '@glif/filecoin-address'
 import {
   privateKeyContainer,
   PrivateKeyContainer,
 } from './private-key-container'
 
-export class LocalManagedProvider implements WalletSubProvider {
-  private readonly privateKey: PrivateKeyContainer
+export class SingleKeyProvider implements WalletSubProvider {
+  #privateKey: string
+  coinTypeToAddress: Record<CoinType, string> = {
+    [CoinType.MAIN]: '',
+    [CoinType.TEST]: '',
+  }
   readonly type = 'SINGLE_KEY'
-  constructor(privateKey: string, network: Network) {
-    this.privateKey = privateKeyContainer(privateKey, network)
+  constructor(privateKey: string) {
+    if (!privateKey) {
+      throw new errors.InvalidParamsError({
+        message: 'Must pass private key string to single key provider instance',
+      })
+    }
+    this.#privateKey = privateKey
+    this.coinTypeToAddress[CoinType.MAIN] = privateKeyContainer(
+      privateKey,
+      CoinType.MAIN,
+    ).address
+    this.coinTypeToAddress[CoinType.TEST] = privateKeyContainer(
+      privateKey,
+      CoinType.TEST,
+    ).address
   }
 
-  async getAccounts(): Promise<string[]> {
-    return [this.privateKey.address]
+  async getAccounts(
+    _: number,
+    __: number,
+    coinType: CoinType = CoinType.MAIN,
+  ): Promise<string[]> {
+    return [this.coinTypeToAddress[coinType]]
   }
 
   async sign(from: string, message: LotusMessage): Promise<SignedLotusMessage> {
-    if (from === this.privateKey.address) {
-      return this.privateKey.sign(message)
+    let pk: PrivateKeyContainer
+
+    if (this.coinTypeToAddress[CoinType.MAIN] === from) {
+      pk = privateKeyContainer(this.#privateKey, CoinType.MAIN)
+    } else if (this.coinTypeToAddress[CoinType.TEST] === from) {
+      pk = privateKeyContainer(this.#privateKey, CoinType.TEST)
     } else {
-      throw new Error(`Can only sign with address ${this.privateKey.address}`)
+      throw new errors.InvalidParamsError({
+        message: 'Invalid from address for private key',
+      })
     }
+
+    return pk.sign(message)
   }
 }
