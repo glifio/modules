98d580e2dd8117641d4064142017ae197680e545
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-11-23 20:57:16 -0500

    Export single key provider

--- packages/filecoin-wallet-provider/src/index.ts
+++ packages/filecoin-wallet-provider/src/index.ts
@@ -4,6 +4,7 @@ export * from './errors'
 export * from './wallet-sub-provider'
 export * from './providers/hd-wallet-provider'
 export * from './providers/ledger-provider'
+export * from './providers/single-key-provider'
 export * from './types'
 export { default as TransportWrapper } from './TransportWrapper'
 
