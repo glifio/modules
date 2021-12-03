019782d2446fd883114bf1e1ab6c9f05eaef2956
Jon Schwartz <jpschwartz2@uwalumni.com>
2021-12-03 13:44:33 -0500

    Delete react-component library from modules

--- packages/react-components/src/utils/truncateAddress/index.js
+++
@@ -1,7 +0,0 @@
-export default (address, charCountStart, charCountEnd) => {
-  if (address.length <= 12) return address
-
-  return `${address.slice(0, charCountStart || 6)} ... ${address.slice(
-    -charCountEnd || -6
-  )}`
-}
