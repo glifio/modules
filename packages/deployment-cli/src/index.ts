0c18bca5278aeecdefd0008390d9bb4aef714138
Jon <Schwartz10@users.noreply.github.com>
2022-02-21 20:53:06 -0500

    Create a CLI command to upload directory to web3 storage (#164)
    * Create a CLI command to upload directory to web3 storage
    
    * Add cloudflare put
    
    * Delete unused cmd and deps

---
+++ packages/deployment-cli/src/index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import { program } from 'commander'
+import { getFilesFromPath, Web3Storage } from 'web3.storage'
+
+program
+  .command('ipfs <directory> <token>')
+  .description('Store a file path on web3.storage')
+  .action(async (directory: string, token: string) => {
+    const _files = await getFilesFromPath(directory)
+    const files = _files.map((file) => ({
+      ...file,
+      name: `/${file.name.split('/').slice(2).join('/')}`,
+    }))
+    const client = new Web3Storage({ token })
+    // @ts-expect-error
+    const cid = await client.put(files)
+    console.log(cid)
+  })
+
+program.parse()
