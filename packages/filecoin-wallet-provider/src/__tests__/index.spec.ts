9340b7a67b9d248086d35d92edb4d353149b847d
Jon Schwartz <jpschwartz2@uwalumni.com>
2022-07-26 21:03:22 -0700

    Cleanup

--- packages/filecoin-wallet-provider/src/__tests__/index.spec.ts
+++ packages/filecoin-wallet-provider/src/__tests__/index.spec.ts
@@ -15,7 +15,7 @@ import { WalletSubProvider } from '../wallet-sub-provider'
 const testSubProviderInstance: WalletSubProvider = {
   type: 'MOCK',
   getAccounts: jest.fn().mockImplementation(async () => []),
-  keyDerive: jest.fn().mockImplementation(async () => {}),
+  keyDerive: jest.fn().mockImplementation(async () => ''),
   sign: jest.fn().mockImplementation(async () => null),
 }
 
