1a7d69b586777d15c3d61c07d033b615ccc5061f
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-25 23:19:06 +0200

    update tests

--- packages/filecoin-actor-utils/test/index.spec.ts
+++ packages/filecoin-actor-utils/test/index.spec.ts
@@ -1,9 +1,9 @@
 import { networkActorCodeMap } from '../src/data'
-import { getActorName } from '../src/utils'
+import { getActorName, getActorCode } from '../src/utils'
 
 describe('utils', () => {
 
-  describe('getActorCode', () => {
+  describe('getActorName', () => {
 
     test('should find the actor name when given a code and network', () => {
       const code = networkActorCodeMap['mainnet']['multisig']
@@ -22,6 +22,23 @@ describe('utils', () => {
 
     test('should not find the actor name when the code does not exist', () => {
       expect(getActorName("abc123")).toBe(null)
+    })
+  })
+
+  describe('getActorCode', () => {
+
+    test('should find the actor code when given a name and network', () => {
+      const code = networkActorCodeMap['calibrationnet']['multisig']
+      expect(getActorCode('multisig', 'calibrationnet')).toBe(code)
+    })
+
+    test('should find the mainnet actor code when not providing a network', () => {
+      const code = networkActorCodeMap['mainnet']['multisig']
+      expect(getActorCode('multisig')).toBe(code)
+    })
+
+    test('should not find the actor code when the actor name does not exist', () => {
+      expect(getActorCode('test', 'mainnet')).toBe(null)
     })
   })
 })
