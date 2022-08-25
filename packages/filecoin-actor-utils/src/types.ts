73f9b3f471095e87f6d7004514eca5b3a4b00182
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-25 16:59:46 +0200

    Create types.ts

---
+++ packages/filecoin-actor-utils/src/types.ts
@@ -0,0 +1,49 @@
+export type ActorName = string
+export type ActorCode = string
+export type MethodNum = string
+export type NetworkName = string
+export type PropName = string
+
+export type ActorCodeMap = {
+  [actorName: ActorName]: ActorCode
+}
+
+export type NetworkActorCodeMap = {
+  [networkName: NetworkName]: ActorCodeMap
+}
+
+export type DataType = {
+  Type: string
+  Name: string
+  Key?: DataType
+  Contains?: DataType
+  Children?: DataTypeMap
+  Methods?: DataTypeMap
+  Params?: DataType[]
+  Returns?: DataType[]
+  IsVariadic?: boolean
+  ChanDir?: string
+}
+
+export type DataTypeMap = {
+  [name: PropName]: DataType
+}
+
+export type ActorMethod = {
+  Name: string
+  Param: DataType
+  Return: DataType
+}
+
+export type ActorMethodMap = {
+  [methodNum: MethodNum]: ActorMethod
+}
+
+export type ActorDescriptor = {
+  State: DataTypeMap
+  Methods: ActorMethodMap
+}
+
+export type ActorDescriptorMap = {
+  [actorName: ActorName]: ActorDescriptor
+}
