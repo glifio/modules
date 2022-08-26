85baefdbb2fd7908c07a5a6bc02695ca64636399
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-26 14:57:28 +0200

    Create .eslintrc.js

---
+++ packages/filecoin-actor-utils/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true
+      }
+    ]
+  }
+}
