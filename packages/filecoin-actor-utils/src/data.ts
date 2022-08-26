5dfb7378f4f3024d3557f7607d802491bec789b4
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-26 12:01:03 +0200

    Update data.ts

--- packages/filecoin-actor-utils/src/data.ts
+++ packages/filecoin-actor-utils/src/data.ts
@@ -3,4 +3,14 @@ import actorDescriptorMapJSON from '../data/actor-descriptors.json'
 import { NetworkActorCodeMap, ActorDescriptorMap } from './types'
 
 export const networkActorCodeMap = networkActorCodeMapJSON as NetworkActorCodeMap
+export const networkActorCodeMapInv = Object.fromEntries(
+  Object.entries(networkActorCodeMap).map(
+    ([networkName, actorCodeMap]) => [networkName, Object.fromEntries(
+      Object.entries(actorCodeMap).map(
+        ([actorName, actorCode]) => [actorCode, actorName]
+      )
+    )]
+  )
+)
+
 export const actorDescriptorMap = actorDescriptorMapJSON as ActorDescriptorMap
