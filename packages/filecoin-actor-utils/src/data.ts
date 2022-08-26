e55b1ef82fba5fdb6f7880d81639f07410eda762
Thijs van Hoof <thijs.v.h@gmail.com>
2022-08-26 14:50:24 +0200

    linter fixes

--- packages/filecoin-actor-utils/src/data.ts
+++ packages/filecoin-actor-utils/src/data.ts
@@ -2,15 +2,19 @@ import networkActorCodeMapJSON from '../data/actor-codes.json'
 import actorDescriptorMapJSON from '../data/actor-descriptors.json'
 import { NetworkActorCodeMap, ActorDescriptorMap } from './types'
 
-export const networkActorCodeMap = networkActorCodeMapJSON as NetworkActorCodeMap
+export const networkActorCodeMap =
+  networkActorCodeMapJSON as NetworkActorCodeMap
+
 export const networkActorCodeMapInv = Object.fromEntries(
-  Object.entries(networkActorCodeMap).map(
-    ([networkName, actorCodeMap]) => [networkName, Object.fromEntries(
-      Object.entries(actorCodeMap).map(
-        ([actorName, actorCode]) => [actorCode, actorName]
-      )
-    )]
-  )
+  Object.entries(networkActorCodeMap).map(([networkName, actorCodeMap]) => [
+    networkName,
+    Object.fromEntries(
+      Object.entries(actorCodeMap).map(([actorName, actorCode]) => [
+        actorCode,
+        actorName
+      ])
+    )
+  ])
 )
 
 export const actorDescriptorMap = actorDescriptorMapJSON as ActorDescriptorMap
--- packages/filecoin-actor-utils/src/utils.ts
+++ packages/filecoin-actor-utils/src/utils.ts
@@ -1,21 +1,23 @@
-import { networkActorCodeMap, networkActorCodeMapInv } from "./data"
-import { ActorCode, ActorName, NetworkName } from "./types"
+import { networkActorCodeMap, networkActorCodeMapInv } from './data'
+import { ActorCode, ActorName, NetworkName } from './types'
 
 /**
  * Resolves the actor name by providing the actor code. When also providing
  * the network name, it will limit the search to that specific network.
  * @param actorCode the actor code which is used to resolve the actor name
- * @param networkName (optional) the network in which to search for the actor name 
+ * @param networkName (optional) the network in which to search for the actor name
  * @returns the actor name when found or null when not found
  */
-export const getActorName = (actorCode: ActorCode, networkName?: NetworkName): ActorName | null => {
-  
+export const getActorName = (
+  actorCode: ActorCode,
+  networkName?: NetworkName
+): ActorName | null => {
   // If "networkName" is provided, return the actor name
   // for that specific network or null when not found
   if (networkName) {
     return networkActorCodeMapInv[networkName]?.[actorCode] ?? null
   }
-  
+
   // Otherwise, iterate over all the networks to find the actor name
   for (const network of Object.keys(networkActorCodeMap)) {
     const name = getActorName(actorCode, network)
@@ -33,8 +35,10 @@ export const getActorName = (actorCode: ActorCode, networkName?: NetworkName): A
  * @param networkName (optional) the network in which to search for the actor code, defaults to "mainnet"
  * @returns the actor code when found or null when not found
  */
-export const getActorCode = (actorName: ActorName, networkName: NetworkName = 'mainnet'): ActorCode | null => {
-
+export const getActorCode = (
+  actorName: ActorName,
+  networkName: NetworkName = 'mainnet'
+): ActorCode | null => {
   // Return null when the actor code is not found
   return networkActorCodeMap[networkName]?.[actorName] ?? null
 }
