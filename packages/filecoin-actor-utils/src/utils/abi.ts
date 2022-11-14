249df467aa74b7a75adbe4240ea1715aa729598a
Thijs van Hoof <thijs.v.h@gmail.com>
2022-11-14 15:08:22 +0100

    Update ABI type

--- packages/filecoin-actor-utils/src/utils/abi.ts
+++ packages/filecoin-actor-utils/src/utils/abi.ts
@@ -2,12 +2,11 @@ import { Fragment, JsonFragment } from '@ethersproject/abi'
 import { fromString, toString } from 'uint8arrays'
 import { decode } from '@ipld/dag-cbor'
 
-export type ABI = ReadonlyArray<Fragment | JsonFragment | string>
+export type ABI = string | ReadonlyArray<Fragment | JsonFragment | string>
 
 /**
  * Returns a hex representation of a base64 encoded cbor string
  */
-
 export const cborToHex = (base64: string): string => {
   const data = fromString(base64, 'base64')
   const decoded = decode<Uint8Array>(data)
