0b4a1ce186c0317d89732270abac605222ddb266
Thijs van Hoof <thijs.v.h@gmail.com>
2022-11-16 11:41:13 +0100

    getFEVMMethodName

--- packages/filecoin-actor-utils/src/utils/method.ts
+++ packages/filecoin-actor-utils/src/utils/method.ts
@@ -1,4 +1,6 @@
+import { ethers } from 'ethers'
 import { actorDescriptorMap } from '../data'
+import { ABI, cborToHex } from './abi'
 
 /**
  * Resolves the method name by providing the actor name and the method number.
@@ -11,3 +13,15 @@ export const getMethodName = (
   methodNum: number
 ): string | null =>
   actorDescriptorMap[actorName]?.Methods[methodNum]?.Name ?? null
+
+/**
+ * Resolves the FEVM method name by parsing the base64 params string using the ABI.
+ * @param params the base64 encoded `params` of a Filecoin / Lotus message
+ * @param abi the ABI of the contract that lives at the `to` address of the message
+ * @returns the method name
+ */
+export const getFEVMMethodName = (params: string, abi: ABI): string => {
+  const iface = new ethers.utils.Interface(abi)
+  const data = cborToHex(params)
+  return iface.parseTransaction({ data }).name
+}
