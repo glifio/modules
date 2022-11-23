82d6169740fb1f6c7194ba1c4ad7e52e64ae8120
Thijs van Hoof <thijs.v.h@gmail.com>
2022-11-23 22:00:28 +0100

    Update jest.config.js

--- packages/filecoin-message/jest.config.js
+++ packages/filecoin-message/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    'multiformats/basics':
+      '<rootDir>/node_modules/multiformats/cjs/src/basics.js',
+    'multiformats/cid': '<rootDir>/node_modules/multiformats/cjs/src/cid.js',
+    multiformats: '<rootDir>/node_modules/multiformats/cjs/src/index.js',
+  },
 };
