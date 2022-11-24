64c4540e94071759fdfc1e03657ba97592ca91c7
Thijs van Hoof <thijs.v.h@gmail.com>
2022-11-24 05:39:28 +0100

    Eth hash from cid (#261)
    * Install multiformats
    
    * Update jest.config.js
    
    * install uint8arrays
    
    * Update jest.config.js
    
    * getEthHexFromCid
    
    * Update jest.config.js
    
    * Update index.ts
    
    * linter fix
    
    * Add tests
    
    * Rename functions
    
    Co-authored-by: Jon Schwartz <jpschwartz2@uwalumni.com>

--- packages/filecoin-message/jest.config.js
+++ packages/filecoin-message/jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  moduleNameMapper: {
+    'multiformats/basics':
+      '<rootDir>/node_modules/multiformats/cjs/src/basics.js',
+    'multiformats/cid': '<rootDir>/node_modules/multiformats/cjs/src/cid.js',
+    'multiformats/hashes/digest':
+      '<rootDir>/node_modules/multiformats/cjs/src/hashes/digest.js',
+    multiformats: '<rootDir>/node_modules/multiformats/cjs/src/index.js',
+    uint8arrays: '<rootDir>/node_modules/uint8arrays/cjs/src/index.js'
+  }
+}
