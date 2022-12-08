a5bdcbd5369a42849436aa1c4916f99d1c39ef95
Thijs van Hoof <thijs.v.h@gmail.com>
2022-12-08 12:39:39 +0100

    linter fix

--- packages/filecoin-actor-utils/src/data.ts
+++ packages/filecoin-actor-utils/src/data.ts
@@ -30,7 +30,7 @@ export const networkActorCodeMapInv: NetworkActorCodeMapInv =
 /**
  * Returns the actor descriptors with MethodNum converted to number from string.
  * This conversion is necessary because JSON does not allow numerical object keys.
- * 
+ *
  * Note: Using type assertion for `ActorDescriptorMap` here, because the JSON string
  * values of the `Type` property in `DataType` are incompatible with the `Type` enum.
  */
